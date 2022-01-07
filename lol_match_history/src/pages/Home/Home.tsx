cb01b1b93d9569ccbac13bbd18e02ef0b9148d8c
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-01-07 19:36:19 +0900

    홈화면 완료

--- lol_match_history/src/pages/Home/Home.tsx
+++ lol_match_history/src/pages/Home/Home.tsx
@@ -1,18 +1,29 @@
 import React, { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import bgImage from "../../assets/images/main-bg.png";
 
 const Home = (): JSX.Element => {
     const [username, setUsername] = useState("");
+    const navigate = useNavigate();
+
     const onInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
         setUsername(event.target.value);
     }
 
+    const onSubmit = (): void => {
+        if (username !== "") {
+            navigate(`/histories/${username}`);
+        }
+    }
+
     return (
         <div className='home'>
+            <img src={bgImage} alt="포로" className="home__bg-image" />
             <div className="home__header">
                 <h1 className='header__title'>LOLLY</h1>
                 <h4 className='header__subtitle'>국내 no.1과는 거리가 먼 롤 전적 검색 사이트</h4>
             </div>
-            <form className='home__form'>
+            <form className='home__form' onSubmit={onSubmit}>
                 <input
                     className='home__input'
                     placeholder='검색하고자 하는 소환사명을 입력하세요.'
@@ -22,8 +33,7 @@ const Home = (): JSX.Element => {
                 <button
                     type="submit"
                     className='home__submit button'
-                    onSubmit={() => 1}
-                />
+                >검색</button>
             </form>
         </div>
     )
