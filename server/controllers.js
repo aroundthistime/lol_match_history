453deab5b683dc58b7ab9827cfa5377071b2f3cb
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-01-11 16:33:36 +0900

    룬특성 정보 가져 진행중

--- server/controllers.js
+++ server/controllers.js
@@ -1,8 +1,51 @@
+import { getChampionImageUrl, getSummonerSpellImageUrl, getTierImageUrl } from "./imageExtractors";
+
 const { default: axios } = require("axios");
 
 const RANKED_SOLO = 'RANKED_SOLO_5x5';
 const RANKED_TEAM = 'RANKED_TEAM_5x5';
 
+const CODES = {
+    QUEUE_TYPES: {
+        400: 'norm', //Normal Draft Pick
+        420: 'solo',
+        430: 'norm',
+        440: 'flex',
+        450: 'aram',
+        700: 'clash',
+        830: 'ai',
+        840: 'ai',
+        850: 'ai',
+        900: 'urf',
+        920: 'poro',
+        1020: 'ofa',
+        1300: 'nbg',
+        1400: 'usb', // Ultimate Spellbook
+        2000: 'tut',
+        2010: 'tut',
+        2020: 'tut',
+    },
+    TEAM_ID: {
+        blue: 100,
+        red: 200
+    }
+}
+
+const QUEUE_TYPES_KOREAN = {
+    "solo": "솔랭",
+    "norm": "일반",
+    "aram": "칼바람",
+    "flex": "자랭",
+    "nbg": "돌넥",
+    "usb": "궁주문서",
+    "urf": "URF",
+    "ofa": "단일",
+    "ai": "AI대전",
+    "poro": "포로왕",
+    "tut": "튜토리얼",
+    "etc": "기타",
+    "clash": "격전"
+}
 
 const fetchUser = async (username) => {
     try {
@@ -33,10 +76,6 @@ const fetchUser = async (username) => {
             result: false
         })
     }
-}
-
-const getTierImageUrl = (tier) => {
-    return `http://127.0.0.1:${process.env.PORT}/static/img/tiers/${tier}.png`
 }
 
 const extractTier = async (tiers, isSolo) => {
@@ -79,7 +118,93 @@ const fetchUserTiers = async (summonerId) => {
     } catch (error) {
         console.log(error);
     }
+}
 
+const getSummonerSpellImages = async (players) => {
+    const summonerSpellResponse = await axios.get("https://ddragon.leagueoflegends.com/cdn/10.6.1/data/en_US/summoner.json");
+    try {
+        const summonerSpellJson = summonerSpellResponse.data.data;
+        return Promise.all(players.map(async (player) => {
+            const summonerSpell1Image = await getSummonerSpellImageUrl(`${player.spell1Id}`, summonerSpellJson);//json에는 spell ID가 string으로 저장되어있기 때문에 맞춰서 변환
+            const summonerSpell2Image = await getSummonerSpellImageUrl(`${player.spell2Id}`, summonerSpellJson);
+            return ({
+                ...player,
+                spell1Image: summonerSpell1Image,
+                spell2Image: summonerSpell2Image
+            });
+        }))
+    } catch (error) {
+        return false
+    }
+}
+
+const getChampionImages = async (players) => {
+    const championResponse = await axios.get("http://ddragon.leagueoflegends.com/cdn/12.1.1/data/en_US/champion.json");
+    try {
+        const championJson = championResponse.data.data;
+        return Promise.all(players.map(async (player) => {
+            const championImage = await getChampionImageUrl(`${player.championId}`, championJson); //json에는 champion ID가 string으로 저장되어있기 때문에 맞춰서 변환
+            return ({
+                ...player,
+                championImage
+            });
+        }))
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+const getRunes = async (players) => {
+    const response = await axios.get('https://ddragon.leagueoflegends.com/cdn/10.6.1/data/en_US/runesReforged.json');
+    try {
+        const runeDatas = response.data;
+        return Promise.all(players.map(async (player) => {
+            const mainRunes = await getChampionRunes(player.perks, perkIds, player.perks.perkStyle, runeDatas);
+            const subRunes = await getChampionRunes(player.perks, perkIds, player.perks.perkSubStyle, runeDatas);
+            return ({
+                ...player,
+                championImage
+            });
+        }))
+    } catch (error) {
+        return false;
+    }
+}
+
+const extractGamePlayerInfos = async (playersParameter) => {
+    let players = await getSummonerSpellImages(playersParameter);
+    players = await getChampionImages(players);
+    players = await getChampionRunes(players);
+    // console.log(players)
+    const blueTeam = await players.filter(player => player.teamId === CODES.TEAM_ID.blue);
+    const redTeam = await players.filter(player => player.teamId === CODES.TEAM_ID.red);
+    console.log(blueTeam);
+    return ({
+        blue: blueTeam,
+        red: redTeam
+    })
+}
+
+
+const fetchCurrentMatch = async (summonerId) => {
+    try {
+        const response = await axios.get(`https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summonerId}?api_key=${process.env.API_KEY}`);
+        if (response.status !== 200) {
+            throw Error;
+        }
+        const gameModeEng = CODES.QUEUE_TYPES[response.data.gameQueueConfigId];
+        const gameModeKor = QUEUE_TYPES_KOREAN[gameModeEng];
+        const gamePlayers = await extractGamePlayerInfos(response.data.participants);
+        return ({
+            gameLength: response.data.gameLength,
+            gameMode: gameModeKor,
+            players: gamePlayers
+        })
+
+    } catch (error) { //no current games or API KEY expired
+        return undefined
+    }
 }
 
 export const fetchByUsername = async (req, res) => {
@@ -89,11 +214,11 @@ export const fetchByUsername = async (req, res) => {
     let user = await fetchUser(username);
     if (user.result) {
         const tiers = await fetchUserTiers(user.id);
-        console.log("TIERS : ", tiers);
-        // const matches = await fetchLastMatches()
+        const currentMatch = await fetchCurrentMatch(user.id)
         user = {
             ...user,
-            tiers
+            tiers,
+            currentMatch
         }
     }
     res.json(user);
---
+++ server/imageExtractors.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+export const getTierImageUrl = (tier) => {
+    return `http://127.0.0.1:${process.env.PORT}/static/img/tiers/${tier}.png`
+}
+
+export const getSummonerSpellImageUrl = async (spellId, summonerSpellJson) => {
+    try {
+        let summonerSpell = Object.entries(summonerSpellJson).find(entry => {
+            const [spellName, spellObj] = entry;
+            return spellObj.key === spellId;
+        })
+        const summonerSpellName = summonerSpell[0];
+        return `http://ddragon.leagueoflegends.com/cdn/12.1.1/img/spell/${summonerSpellName}.png`;
+    } catch (error) {
+        return false;
+    }
+}
+
+export const getChampionImageUrl = async (championId, championJson) => {
+    try {
+        let champion = Object.entries(championJson).find(entry => {
+            const [championName, championObj] = entry;
+            return championObj.key === championId;
+        })
+        const championName = champion[0];
+        return `http://ddragon.leagueoflegends.com/cdn/12.1.1/img/champion/${championName}.png`;
+    } catch (error) {
+        return false;
+    }
+}
+
+export const getChampionRunes = async (runes, runeStyle, runeDatas) => {
+    try {
+        let currentRuneStyle = runeDatas.find(runeStyleObj => runeStyleObj.id === runeStyle);
+        let selectedRunes = await runes.filter(rune =>
+            currentRuneStyle.slots.some(runeOfCurrentStyle => runeOfCurrentStyle.id === rune.id));
+        selectedRunes = await selectedRunes.map(selectedRune => ({
+            ...selectedRune,
+            image: `http://127.0.0.1:${process.env.PORT}/static/img/${selectedRune.icon}`
+        }));
+
+        return
+    } catch (error) {
+        return false
+    }
+}
