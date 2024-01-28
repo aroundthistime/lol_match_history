6a2240c23054d4e9c909c5ef1f61b1e5915fdb4e
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2024-01-28 21:42:54 +0900

    style: Create logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Logo from '../src/components/Logo';
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <main className='flex justify-center items-center'>
+            <Logo className='text-5xl mb-50' />
+        </main>
+    )
 }
